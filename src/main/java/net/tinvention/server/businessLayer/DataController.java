3b8354ba1c06ddc09c66ab168d3671b76eff06ea
Andrea Delfino <a.delfino@reply.it>
2014-09-13 20:38:52 +0200

    Lista di eventi a frontend

--- src/main/java/net/tinvention/server/businessLayer/DataController.java
+++ src/main/java/net/tinvention/server/businessLayer/DataController.java
@@ -1,11 +1,13 @@
 package net.tinvention.server.businessLayer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import net.tinvention.server.dataLayer.DataManager;
 import net.tinvention.server.model.DataRaw;
+import net.tinvention.server.model.Event;
 import net.tinvention.server.model.EventType;
 
 public class DataController {
@@ -13,7 +15,60 @@ public class DataController {
 	@Autowired
 	private DataManager dm;
 	
-	public List<DataRaw> getLastTypeValues(EventType type, int numberOfResults){
+	public List<List<Event>> getEvents(){
+		List<List<Event>> eventsList = new ArrayList<>();
+		
+		List<Event> ACC = new ArrayList<>();
+		for(DataRaw raw : dm.GetDataRaw(EventType.ACC, 50)){ //Ultimi 50 elementi per tipo
+			Event ev = new Event();
+			ev.setType(EventType.ACC);
+			ev.setTimestamp(raw.getTimestamp());
+			ev.setAccX(raw.getAccX());
+			ev.setAccY(raw.getAccY());
+			ev.setAccZ(raw.getAccZ());
+		}
+		eventsList.add(ACC);
+		
+		List<Event> TEMP = new ArrayList<>();
+		for(DataRaw raw : dm.GetDataRaw(EventType.TEMP, 50)){ //Ultimi 50 elementi per tipo
+			Event ev = new Event();
+			ev.setType(EventType.TEMP);
+			ev.setTimestamp(raw.getTimestamp());
+			ev.setValue(raw.getValue());
+		}
+		eventsList.add(TEMP);
+		
+		List<Event> BVP = new ArrayList<>();
+		for(DataRaw raw : dm.GetDataRaw(EventType.BVP, 50)){ //Ultimi 50 elementi per tipo
+			Event ev = new Event();
+			ev.setType(EventType.BVP);
+			ev.setTimestamp(raw.getTimestamp());
+			ev.setValue(raw.getValue());
+		}
+		eventsList.add(BVP);
+		
+		List<Event> GSR = new ArrayList<>();
+		for(DataRaw raw : dm.GetDataRaw(EventType.GSR, 50)){ //Ultimi 50 elementi per tipo
+			Event ev = new Event();
+			ev.setType(EventType.GSR);
+			ev.setTimestamp(raw.getTimestamp());
+			ev.setValue(raw.getValue());
+		}
+		eventsList.add(GSR);
+		
+		List<Event> IBI = new ArrayList<>();
+		for(DataRaw raw : dm.GetDataRaw(EventType.IBI, 50)){ //Ultimi 50 elementi per tipo
+			Event ev = new Event();
+			ev.setType(EventType.IBI);
+			ev.setTimestamp(raw.getTimestamp());
+			ev.setValue(raw.getValue());
+		}
+		eventsList.add(IBI);
+		
+		return eventsList;
+	}
+	
+	 List<DataRaw> getLastTypeValues(EventType type, int numberOfResults){
 		return this.dm.GetDataRaw();
 	}
 	
--- src/main/java/net/tinvention/server/controller/IndexController.java
+++ src/main/java/net/tinvention/server/controller/IndexController.java
@@ -1,10 +1,17 @@
 package net.tinvention.server.controller;
 
+import java.util.List;
+
+import net.tinvention.server.businessLayer.DataController;
+import net.tinvention.server.model.Event;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
@@ -13,9 +20,19 @@ public class IndexController {
 
 		//@Autowired
 		//private ArduinoMexManager arduinoMexManager;
+	
+		@Autowired
+		private DataController dc;
 			
 		@RequestMapping(value="/index.html", method = RequestMethod.GET)
 		public String welcome(ModelMap model) throws InterruptedException {
 			return "index";
 		}
-}
+		
+		
+		@RequestMapping(value = "/eventList", method = RequestMethod.GET, produces = { "application/json" })
+		@ResponseStatus(HttpStatus.OK)
+		public List<List<Event>> getEventsList() {
+			return dc.getEvents();
+		}
+	}
